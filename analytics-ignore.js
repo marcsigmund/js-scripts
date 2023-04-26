b5cc964c615ed302bd9b2dadc2c83f93118cad92
marcsigmund <73389028+marcsigmund@users.noreply.github.com>
2023-04-26 13:36:22 +0200

    ok

---
+++ analytics-ignore.js
@@ -0,0 +1,3 @@
+if (location.search.indexOf("analytics_ignore") !== -1) {
+  localStorage.setItem("plausible_ignore", true)
+}
--- plausible-ignore.js
+++
@@ -1,3 +0,0 @@
-if (location.search.indexOf("plausible_ignore") !== -1) {
-  localStorage.setItem("plausible_ignore", true)
-}
